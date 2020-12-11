e557b749c8f5aca31797a943f57adfff81beffc3
yvikasofficial <yvikasofficial@gmail.com>
2020-12-11 18:54:19 +0530

    Refcatored Home Componets, Added Styling to Home items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./homepage.componet";
+import HomePage from "./pages/homepage/homepage.componet";
 
 function App() {
   return (
