e1d4dddfc1aaf27e986e59891c55e2c3a230ccdd
yvikasofficial <yvikasofficial@gmail.com>
2020-12-12 03:45:08 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.componet";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route>
+        <Route exact path="/" component={HomePage} />
+      </Route>
     </div>
   );
 }
--- src/pages/homepage/homepage.componet.jsx
+++ src/pages/homepage/homepage.componet.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Directory from "../../components/directory/directory.componet";
 import "./homepage.styles.scss";
 
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <div className="homepage">
       <Directory />
