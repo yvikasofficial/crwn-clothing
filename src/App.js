657d694ccff159ebc206a69de1f429608d06817b
yvikasofficial <yvikasofficial@gmail.com>
2020-12-13 19:55:48 +0530

    Added Signup with email & password

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.componet";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.componet";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
   state = {
@@ -15,12 +15,26 @@ class App extends Component {
 
   unsubscribeFromAuth = null;
 
-  componentDidMount() {
-    auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+  componentDidMount = () => {
+    auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapshot.id,
+                ...snapshot.data(),
+              },
+            },
+            () => console.log(this.state)
+          );
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
-  }
+  };
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.componet.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.componet.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in.componet";
+import SingUp from "../../components/sign-up/sign-up.componet";
 
 import "./sign-in-and-sign-up.styles.scss";
 
@@ -7,6 +8,7 @@ const SignInAndSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SingUp />
     </div>
   );
 };
