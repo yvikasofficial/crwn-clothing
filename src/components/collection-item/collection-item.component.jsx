c23d8b725918d72df0406e7edc119b3c63c98f40
yvikasofficial <yvikasofficial@gmail.com>
2020-12-16 19:13:03 +0530

    Upadated Cart data, Added Utils for cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
-import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.comoponet";
+import { addItem } from "../../redux/cart/cart.actions";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+import "./collection-item.styles.scss";
+import { connect } from "react-redux";
+
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -10,8 +15,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name"> {name} </span>
         <span className="price"> {price} </span>
       </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add To Cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,10 @@
+import { addItemToCart } from "./cart.utils";
+
 const { default: CartActionTypes } = require("./cart.types");
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -10,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         hidden: !state.hidden,
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
 
     default:
